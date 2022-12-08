c6642ad18d11fae023c9cc75df5f34c8b5aa11c6
vladyslav <lohegor9@gmail.com>
2022-12-08 20:14:31 +0200

    Добавьте файлы проекта.

---
+++ NumberToCode.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void convertNumber(int number)
+{
+    for (int i=134217728; i>0; i/=2)
+    {
+        if (i == 8388608 or i == 524288 or i == 32768 or i == 2048 or i== 128 or i == 8)
+            std::cout << " ";
+
+        if (number >= i)
+        {
+            number -= i;
+            std::cout << true;
+        }
+        else
+            std::cout << false;
+
+    }
+    std::cout << "\n";
+}
+
+int main()
+{
+    std::cout << "Enter a number from 0 to 134 217 728: ";
+    int number;
+    std::cin >> number;
+
+    convertNumber(number);
+    main();
+}
